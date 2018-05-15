b39823c63c1ddad7df9d6c1a179eda5ee0205fb0
Kara Kelley <kelley.15@gmail.com>
2018-05-15 13:04:11 -0500

    Setup ci deployment (#1)
    * Add deployment conditions for Travis
    
    * Update repo for heroku
    
    * Update Procfile
    
    * Fix travis formatting
    
    * Update README

--- src/main/java/me/karakelley/App.java
+++ src/main/java/me/karakelley/App.java
@@ -3,24 +3,19 @@ package me.karakelley;
 
 import me.karakelley.http.application.presenters.HtmlFilePresenter;
 import me.karakelley.http.application.requestmatchers.AlwaysMatchesMatcher;
-import me.karakelley.http.application.requestmatchers.MethodAndExactPathMatcher;
 import me.karakelley.http.server.Handler;
 import me.karakelley.http.server.filesystem.PublicDirectory;
 import me.karakelley.http.server.handlers.Router;
-import me.karakelley.http.server.handlers.StaticFilesHandler;
-import me.karakelley.http.server.http.HttpMethod;
 import me.karakelley.http.server.http.Request;
 import me.karakelley.http.server.http.Response;
 
 public class App implements Handler {
   private final Router router;
 
-  public App(PublicDirectory publicDirectory) {
+  App(PublicDirectory publicDirectory) {
     this.router = new Router()
             .route(new AlwaysMatchesMatcher(), new FilesHandler(publicDirectory, new HtmlFilePresenter(publicDirectory)));
-
   }
-
 
   @Override
   public Response respond(Request request) {
